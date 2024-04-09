2bb63d98c874a962b12adec982f6fa78c55ebcc9
Paul Mooney <paul@daishisystems.com>
2024-04-09 11:16:42 +0100

    Added password complexity function

--- pluralsight-healthcare-cli/src/main/java/com/pluralsight/healthcare/cli/PasswordHasher.java
+++ pluralsight-healthcare-cli/src/main/java/com/pluralsight/healthcare/cli/PasswordHasher.java
@@ -14,5 +14,18 @@ public class PasswordHasher {
 
         LOG.info("Original: {}", originalPassword);
         LOG.info("Hashed: {}", hashedPassword);
+
+        String password = "UtCWg1#&4hdcU7k2";
+        boolean passwordIsComplex = isPasswordComplex(password);
+        if (passwordIsComplex) {
+            LOG.info("{} is a complex password.", password);
+        } else {
+            LOG.warn("{} is not a complex password.", password);
+        }
+    }
+
+    public static boolean isPasswordComplex(String password) {
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$";
+        return password.matches(regex);
     }
 }
