7375a2c8e15ed63771d4e673439563c46331de0f
Paul Mooney <paul@daishisystems.com>
2024-04-16 15:10:58 +0100

    Added JWT session cookie

--- pluralsight-healthcare-server/src/main/java/com/pluralsight/healthcare/server/PatientResource.java
+++ pluralsight-healthcare-server/src/main/java/com/pluralsight/healthcare/server/PatientResource.java
@@ -4,15 +4,15 @@ import com.pluralsight.healthcare.domain.Patient;
 import com.pluralsight.healthcare.repository.PatientRepository;
 import com.pluralsight.healthcare.repository.RepositoryException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 import org.owasp.html.PolicyFactory;
 import org.owasp.html.Sanitizers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.stream.Stream;
@@ -20,7 +20,7 @@ import java.util.stream.Stream;
 @Path("/patients")
 public class PatientResource {
     private static final Logger LOG = LoggerFactory.getLogger(PatientResource.class);
-    private static final String SECRET_KEY = "secretKey"; // This should be managed securely
+    private static final String SECRET_KEY = "eW91ci1iYXNlNjQtZW5jb2RlZC1zZWNyZXQta2V5LWhlcmU="; // This should be managed securely
 
     private final PatientRepository patientRepository;
 
@@ -128,11 +128,13 @@ public class PatientResource {
     private String generateJWT() {
         long nowMillis = System.currentTimeMillis();
         long expMillis = nowMillis + 3600000; // Token is valid for 1 hour
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
+
         return Jwts.builder()
-                .subject("")
+                .subject("subject")
                 .issuedAt(new Date(nowMillis))
                 .expiration(new Date(expMillis))
-                .signWith(new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "AES"))
+                .signWith(key)
                 .compact();
     }
 }
